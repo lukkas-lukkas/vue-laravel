fe80ff22ade0a597e14e16fb54769c1d77cff739
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-12 17:41:11 -0300

    auth api with bearer token

--- application/routes/api.php
+++ application/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\TokenController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/get-token', [TokenController::class, 'getToken']);
 
-Route::apiResource('client', ClientController::class);
+Route::apiResource('client', ClientController::class)->middleware('api.auth');
